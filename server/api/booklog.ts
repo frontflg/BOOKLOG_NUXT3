6e1c1e2cf2b3b7f95d5446637de46bb12113de88
frontflg <50544382+frontflg@users.noreply.github.com>
2023-05-01 07:59:58 +0900

    Create booklog.ts
    PrismaClient設定（BOOKLOG用CRUD）

---
+++ server/api/booklog.ts
@@ -0,0 +1,87 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+import { sendError } from 'h3';
+
+export default defineEventHandler(async (e) => {
+  const prisma = new PrismaClient();
+  const method = e.req.method;
+
+  if (method === 'GET') {
+    // console.log(method);
+    const booklog = await prisma.booklog.findMany({
+      orderBy: { issuedate: 'desc' },
+    });
+
+    return booklog;
+  }
+
+  if (method === 'POST') {
+    const body = await readBody(e);
+    console.log('post', body);
+    if (!body) {
+      const detailError = createError({
+        statusCode: 400,
+        statusMessage: 'No item provided',
+        data: {},
+      });
+      sendError(e, detailError);
+    }
+
+    let temp = {};
+    try {
+      if (body.mode === 'create') {
+        temp = await prisma.booklog.create({
+          data: body.data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    return temp;
+  }
+
+  if (method === 'PUT') {
+    console.log('update');
+    const body = await readBody(e);
+    if (!body) {
+      const detailError = createError({
+        statusCode: 400,
+        statusMessage: 'No item provided',
+        data: {},
+      });
+
+      sendError(e, detailError);
+    }
+
+    let temp = {};
+    if (body.mode === 'update') {
+      temp = await prisma.booklog.update({
+        where: { isbn13: body.targetId },
+        data: body.data,
+      });
+    }
+    return temp;
+  }
+
+  if (method === 'DELETE') {
+    console.log('delete');
+    const body = await readBody(e);
+    if (!body) {
+      const detailError = createError({
+        statusCode: 400,
+        statusMessage: 'No item provided',
+        data: {},
+      });
+
+      sendError(e, detailError);
+    }
+    let temp = {};
+    if (body.mode === 'delete') {
+      temp = await prisma.booklog.delete({
+        where: { isbn13: body.targetId },
+      });
+    }
+
+    return temp;
+  }
+});
