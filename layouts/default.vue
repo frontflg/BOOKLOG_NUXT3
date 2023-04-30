0dba05ff199240a7d2a0cb0a4666ae5fb061f3f2
frontflg <50544382+frontflg@users.noreply.github.com>
2023-05-01 07:50:26 +0900

    Add files via upload
    【参考】https://zenn.dev/coedo/articles/nuxt3-vuetify3　より　

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,134 +1,72 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+  <v-app>
+    <!-- ヘッダー -->
+    <v-app-bar
       app
+      flat
+      border
+      class="px-md-6"
+    >
+      <v-app-bar-nav-icon
+        @click="switchNavigationDisplay"
+        class="mr-md-4"
+      />
+
+      <v-app-bar-title class="font-weight-bold">
+        Nuxt3 + Vuetify3 🚀
+      </v-app-bar-title>
+
+      <v-spacer />
+
+      <v-icon icon="mdi-account" class="mr-1" />
+      <p class="font-weight-bold">demo.user</p>
+    </v-app-bar>
+    <!-- /ヘッダー -->
+
+    <!-- ナビゲーション -->
+    <v-navigation-drawer
+      v-model="isNavigationShown"
+      app
+      floating
+      class="px-6 py-4"
+      :width="180"
     >
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+          v-for="item in $C.NAVIGATION_ITEMS"
+          active-color="primary"
+          :to="item.path"
+          class="py-3"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
+          <v-icon :icon="item.icon" class="mr-6" />
+          <v-list-item-title
+            class="font-weight-bold"
+            v-text="item.title"
+          />
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <!-- /ナビゲーション -->
+
     <v-main>
-      <v-container>
-        <Nuxt />
+      <v-container class="pa-6 pa-md-9">
+        <slot />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'BOOKLOG',
-          to: '/booklog'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'BOOK',
-          to: '/book'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: '読書履歴管理システム'
-    }
-  }
+<script setup lang="ts">
+// ナビゲーションの表示・非表示を制御
+const isNavigationShown = useState('isNavigationShown', () => true)
+const switchNavigationDisplay = () => {
+  isNavigationShown.value = !isNavigationShown.value
 }
 </script>
 
-<style scoped>
-.v-application {
-  background-color: #e6f2ff;
+<style scoped lang="scss">
+// .v-list-item がアクティブ時の背景色を消す方法が分からなかったので無理やり対応😭
+::v-deep(.v-list-item__overlay) {
+  display: none;
 }
 </style>
