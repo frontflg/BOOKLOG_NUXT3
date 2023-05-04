231c40426737642eab402a5ba8b31d2fbb980730
frontflg <50544382+frontflg@users.noreply.github.com>
2023-05-04 13:49:42 +0900

    Update booklog.vue

--- pages/booklog.vue
+++ pages/booklog.vue
@@ -5,27 +5,27 @@
       <thead>
         <tr>
           <th width= "26px">No.</th>
-          <th width="131px">ISBN13</th>
+          <th width="131px">&nbsp;ISBN13</th>
           <th width="361px" align="left">書籍名</th>
           <th width="221px" align="left">著者</th>
           <th width="151px" align="left">出版社</th>
-          <th width="81px"  align="left">価格</th>
+          <th width="81px"  align="left">&nbsp;価格</th>
           <th width="151px" align="left">分類</th>
-          <th width="111px" align="left">発行日</th>
+          <th width="111px" align="left">&nbsp;発行日</th>
         </tr></thead>
       <tbody>
         <tr v-for="(book,i) in books" :key="book.isbn13">
-          <td align="right">{{ i + 1 }}　</td>
-          <td><NuxtLink :to="`/book/${book.isbn13}`">{{ book.isbn13 }}</NuxtLink>　</td>
+          <td align="right">{{ i + 1 }}&nbsp;</td>
+          <td><NuxtLink :to="`/book/${book.isbn13}`">&nbsp;{{ book.isbn13 }}</NuxtLink>&nbsp;</td>
           <td>{{ book.bookname }}</td>
           <td>{{ book.author }}</td>
           <td>{{ book.publisher }}</td>
-          <td v-if="book.purchase > 0">\{{ book.purchase }}-</td>
-          <td v-if="book.purchase == 0">図書館</td>
-          <td v-if="book.purchase == null">　</td>
+          <td v-if="book.purchase > 0">&nbsp;\{{ book.purchase }}-</td>
+          <td v-if="book.purchase == 0">&nbsp;図書館</td>
+          <td v-if="book.purchase == null">&nbsp;</td>
           <td>{{ book.genre }}</td>
-          <td v-if="book.issuedate !== null">{{ book.issuedate.slice(0,10) }}</td>
-          <td v-if="book.issuedate == null">　</td>
+          <td v-if="book.issuedate !== null">&nbsp;{{ book.issuedate.slice(0,10) }}</td>
+          <td v-if="book.issuedate == null">&nbsp;</td>
         </tr>
       </tbody>
     </table>
