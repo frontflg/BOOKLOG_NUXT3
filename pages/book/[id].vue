81631e50ad1b86e1d080facdfd275a148d987a17
frontflg <50544382+frontflg@users.noreply.github.com>
2023-05-05 06:04:00 +0900

    Update [id].vue

--- pages/book/[id].vue
+++ pages/book/[id].vue
@@ -1,23 +1,23 @@
 <template>
   <v-app>
-    <p class="mb-10">読書履歴（{{ books.isbn13 }}）</p>
+    <p class="mb-10">読書履歴（{{ book.isbn13 }}）</p>
     <table>
-      <tr><th width="75">ISBN13</th><td width="600">&nbsp;{{ books.isbn13 }}</td><th width="300">表　紙</th></tr>
-      <tr><th>ISBN10</th><td>&nbsp;{{ books.isbn10 }}</td>
-          <td rowspan="14" align="center"><v-img max-height="500" max-width="300" :src="`https://images-fe.ssl-images-amazon.com/images/P/${books.isbn10}.09.LZZZZZZZ`" /></td></tr>
-      <tr><th>書　名</th><td>&nbsp;{{ books.bookname }}</td></tr>
-      <tr><th>著　者</th><td>&nbsp;{{ books.author }}</td></tr>
-      <tr><th>出版社</th><td>&nbsp;{{ books.publisher }}</td></tr>
-      <tr><th>分　類</th><td>&nbsp;{{ books.genre }}</td></tr>
-      <tr><th>発行日</th><td><div v-if="books.issuedate !== null">&nbsp;{{ books.issuedate.slice(0,10) }}</div></td></tr>
-      <tr><th>取得日</th><td><div v-if="books.getdate !== null">&nbsp;{{ books.getdate.slice(0,10) }}</div></td></tr>
-      <tr><th>読了日</th><td><div v-if="books.readdate !== null">&nbsp;{{ books.readdate.slice(0,10) }}</div></td></tr>
-      <tr><th>所　有</th><td><div v-if="books.ownership == '1'">&nbsp;所有</div></td></tr>
-      <tr><th>価　格</th><td><div v-if="books.purchase == 0">&nbsp;図書館</div><div v-if="books.purchase !== 0">&nbsp;\{{ books.purchase }}-</div></td></tr>
-      <tr><th>取得元</th><td>&nbsp;{{ books.library }}</td></tr>
-      <tr><th>概　要</th><td>&nbsp;{{ books.overview }}</td></tr>
-      <tr><th>感　想</th><td>&nbsp;{{ books.impressions }}</td></tr>
-      <tr><th>状　態</th><td>&nbsp;{{ books.state }}</td></tr>
+      <tr><th width="75">ISBN13</th><td width="600">&nbsp;{{ book.isbn13 }}</td><th width="300">表　紙</th></tr>
+      <tr><th>ISBN10</th><td>&nbsp;{{ book.isbn10 }}</td>
+          <td rowspan="14" align="center"><v-img max-height="500" max-width="300" :src="`https://images-fe.ssl-images-amazon.com/images/P/${book.isbn10}.09.LZZZZZZZ`" /></td></tr>
+      <tr><th>書　名</th><td>&nbsp;{{ book.bookname }}</td></tr>
+      <tr><th>著　者</th><td>&nbsp;{{ book.author }}</td></tr>
+      <tr><th>出版社</th><td>&nbsp;{{ book.publisher }}</td></tr>
+      <tr><th>分　類</th><td>&nbsp;{{ book.genre }}</td></tr>
+      <tr><th>発行日</th><td><div v-if="book.issuedate !== null">&nbsp;{{ book.issuedate.slice(0,10) }}</div></td></tr>
+      <tr><th>取得日</th><td><div v-if="book.getdate !== null">&nbsp;{{ book.getdate.slice(0,10) }}</div></td></tr>
+      <tr><th>読了日</th><td><div v-if="book.readdate !== null">&nbsp;{{ book.readdate.slice(0,10) }}</div></td></tr>
+      <tr><th>所　有</th><td><div v-if="book.ownership == '1'">&nbsp;所有</div></td></tr>
+      <tr><th>価　格</th><td><div v-if="book.purchase == 0">&nbsp;図書館</div><div v-if="book.purchase !== 0">&nbsp;\{{ book.purchase }}-</div></td></tr>
+      <tr><th>取得元</th><td>&nbsp;{{ book.library }}</td></tr>
+      <tr><th>概　要</th><td>&nbsp;{{ book.overview }}</td></tr>
+      <tr><th>感　想</th><td>&nbsp;{{ book.impressions }}</td></tr>
+      <tr><th>状　態</th><td>&nbsp;{{ book.state }}</td></tr>
     </table>
   </v-app>
 </template>
@@ -25,8 +25,7 @@
 <script setup>
   const route = useRoute();
   const id = route.params.id;
-  const book = ref('');
-  const { data: books, refresh } = useFetch('/api/booklog', {
+  const { data: book, refresh } = useFetch('/api/booklog', {
     method: 'POST',
     body: { mode: 'find', isbn13: id },
   });
@@ -35,4 +34,5 @@
 <style>
   th{ color:#fff; background:#005ab3; position:sticky; top:0; }
   table tr:nth-child(odd){ background:#e6f2ff; }
+  tbody { overflow-x: hidden; overflow-y: scroll; height: 100px; }
 </style>
