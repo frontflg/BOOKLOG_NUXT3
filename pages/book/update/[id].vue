a3d17e15d3fc6cb2bfe96c164aa68377764bd9da
frontflg <50544382+frontflg@users.noreply.github.com>
2023-05-13 14:08:51 +0900

    Update [id].vue

--- pages/book/update/[id].vue
+++ pages/book/update/[id].vue
@@ -1,17 +1,20 @@
 <template>
-  <v-app>
-    <p class="mb-10">読書履歴（{{ book.isbn13 }}）</p>
+  <div v-if="pending">
+    Loading ...
+  </div>
+  <div v-else>
+    <p class="mb-10">読書履歴（{{ book.isbn13 }}）&nbsp;&nbsp;<v-btn @click="refresh">再読込</v-btn></p>
     <table>
       <tr height="40px">
-        <th width="75">ISBN13</th><td>&nbsp;{{ book.isbn13 }}</td>
+        <th width="75">ISBN13</th><td width="160">&nbsp;&nbsp;&nbsp;{{ book.isbn13 }}</td>
         <th width="75">ISBN10</th>
-        <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="isbn10Text">
+        <td width="170">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.isbn10" :value="`${book.isbn10}`">
           </v-text-field>
         </td>
         <th width="75">分　類</th>
-        <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="genreText">
+        <td width="250">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.genre" :value="`${book.genre}`">
           </v-text-field>
         </td>
         <th width="300">表　紙</th>
@@ -19,7 +22,7 @@
       <tr height="40px">
         <th>書　名</th>
         <td colspan="5">
-          <v-text-field clearable density="compact" hide-details="auto" v-model="booknameText">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.bookname" :value="`${book.bookname}`">
           </v-text-field>
         </td>
         <td rowspan="7" align="center" style="padding: 10px;">
@@ -29,64 +32,76 @@
       <tr height="40px">
         <th>著　者</th>
         <td colspan="5">
-          <v-text-field clearable density="compact" hide-details="auto" v-model="authorText">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.author" :value="`${book.author}`">
           </v-text-field>
         </td>
       </tr>
       <tr height="40px">
         <th>出版社</th>
         <td colspan="3">
-          <v-text-field clearable density="compact" hide-details="auto" v-model="publisherText">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.publisher" :value="`${book.publisher}`">
           </v-text-field>
         </td>
         <th>状　態</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="stateNumb">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.state" :value="`${book.state}`">
           </v-text-field>
         </td>
       </tr>
       <tr height="40px">
         <th>発行日</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="issuedateYMD">
-          <!-- div v-if="book.issuedate !== null">&nbsp;{{ book.issuedate.slice(0,10) }}</div -->
-          </v-text-field>
+          <div v-if="`${book.issuedate}` !== null">
+            <v-text-field clearable density="compact" hide-details="auto" v-model="input.issuedate" :value="`${book.issuedate}`.slice(0,10)"></v-text-field>
+          </div>
+            <div v-if="`${book.issuedate}`  == null">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.issuedate"></v-text-field>
+          </div>
         </td>
         <th>取得日</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="getdateYMD">
-          <!-- div v-if="book.getdate !== null">&nbsp;{{ book.getdate.slice(0,10) }}</div -->
-          </v-text-field>
+          <div v-if="`${book.getdate}` !== null">
+            <v-text-field clearable density="compact" hide-details="auto" v-model="input.getdate" :value="`${book.getdate}`.slice(0,10)"></v-text-field>
+          </div>
+          <div v-if="`${book.getdate}`  == null">
+            <v-text-field clearable density="compact" hide-details="auto" v-model="input.getdate"></v-text-field>
+          </div>
         </td>
         <th>読了日</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="readdateYMD">
-          <!-- div v-if="book.readdate !== null">&nbsp;{{ book.readdate.slice(0,10) }}</div -->
-          </v-text-field>
+          <div v-if="`${book.readdate}` !== null">
+            <v-text-field clearable density="compact" hide-details="auto" v-model="input.readdate" :value="`${book.readdate}`.slice(0,10)"></v-text-field>
+          </div>
+          <div v-if="`${book.readdate}`  == null">
+            <v-text-field clearable density="compact" hide-details="auto" v-model="input.readdate"></v-text-field>
+          </div>
         </td>
       </tr>
       <tr height="40px">
         <th>所　有</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="ownershipText">
-          <!-- div v-if="book.ownership == '1'">&nbsp;所有</div -->
-          </v-text-field>
+          <div v-if="`${book.ownership}` !== null">
+            <v-checkbox density="compact" v-model="input.ownership" color="indigo" label="所有" :model-value="true" hide-details></v-checkbox>
+          </div>
+          <div v-if="!`${book.ownership}` == null">
+            <v-checkbox density="compact" v-model="input.ownership" color="indigo" label="所有" :model-value="false" hide-details></v-checkbox>
+          </div>
         </td>
-       <th>価　格</th>
+        <th>価　格</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="purchaseNumb">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.purchase" :value="`${book.purchase}`">
           </v-text-field>
         </td>
         <th>取得元</th>
         <td>
-          <v-text-field clearable density="compact" hide-details="auto" v-model="libraryText">
+          <v-text-field clearable density="compact" hide-details="auto" v-model="input.library" :value="`${book.library}`">
           </v-text-field>
         </td>
       </tr>
       <tr height="40px">
         <th>概　要</th>
         <td colspan="5">
-          <v-text-field clearable hide-details="auto" v-model="overviewText">
+          <v-text-field clearable hide-details="auto" v-model="input.overview" :value="`${book.overview}`">
           </v-text-field>
         </td>
       </tr>
@@ -94,36 +109,44 @@
         <th>感　想</th>
         <td colspan="5">
           <v-textarea clearable density="compact" hide-details="auto" outlined="false"
-            placeholder="感想を書いてください。" v-model="impressionsText"
+            placeholder="感想を書いてください。" v-model="input.impressions" :value="`${book.impressions}`"
           >
           </v-textarea>
         </td>
       </tr>
     </table>
-  </v-app>
+  </div>
 </template>
 
 <script setup>
   const route = useRoute();
+  const input = ref({
+    isbn10:      'ISBN10',
+    genre:       '分類',
+    bookname:    '書名',
+    author:      '著者',
+    publisher:   '発行元',
+    state:       '状態',
+    issuedate:   '2099-99-99',
+    getdate:     '2099-99-99',
+    readdate:    '2099-99-99',
+    ownership:   '所有',
+    purchase:    '価格',
+    library:     '取得元',
+    overview:    '概要',
+    impressions: '感想',
+  });
   const id = route.params.id;
-  const { data: book, refresh } = useFetch('/api/booklog', {
+  const { data: book, pending, refresh } = useFetch('/api/booklog', {
     method: 'POST',
     body: { mode: 'find', isbn13: id },
+    async onResponse({ request, response, options }) {
+      console.log('[fetch response]', request, response.status)
+      for (const header of response.headers.entries()) {
+        console.log('HEADER', header)
+      }
+    },
   });
-  // const isbn10Text      = ref(book.value.isbn10);
-  // const genreText       = ref(book.value.genre);
-  // const booknameText    = ref(book.value.bookname);
-  // const authorText      = ref(book.value.author);
-  // const publisherText   = ref(book.value.publisher);
-  // const stateNumb       = ref(book.value.state);
-  // const issuedateYMD    = ref(book.value.issuedate);
-  // const getdateYMD      = ref(book.value.getddate);
-  // const readdateYMD     = ref(book.value.readdate);
-  // const ownershipText   = ref(book.value.ownership);
-  // const purchaseNumb    = ref(book.value.purchase);
-  // const libraryText     = ref(book.value.library);
-  // const overviewText    = ref(book.value.overview);
-  // const impressionsText = ref(book.value.impressions);
 </script>
 
 <style>
