29e685c6890b973a7146cdb416c303cb5163388d
frontflg <50544382+frontflg@users.noreply.github.com>
2023-06-10 23:19:32 +0900

    Update booklog.vue

--- pages/booklog.vue
+++ pages/booklog.vue
@@ -1,18 +1,17 @@
 <template>
-  <v-app>
-    <p class="mb-10">読書履歴</p>
-    <div style="height:550px; overflow-y:scroll;">
+  <p class="mb-5">読書履歴</p>
+  <div style="height:480px; overflow-y:scroll;">
     <table>
       <thead>
         <tr>
-          <th width="26px">No.</th>
+          <th width= "26px">No.</th>
           <th width="131px">&nbsp;ISBN13</th>
           <th width="361px" align="left">書籍名</th>
           <th width="221px" align="left">著者</th>
           <th width="151px" align="left">出版社</th>
-          <th width="81px"  align="left">&nbsp;価格</th>
+          <th width="81px">&nbsp;価格</th>
           <th width="151px" align="left">分類</th>
-          <th width="116px" align="left">&nbsp;発行日</th>
+          <th width="126px" align="left">&nbsp;発行日</th>
         </tr></thead>
       <tbody>
         <tr v-for="(book,i) in books" :key="book.isbn13">
@@ -21,7 +20,7 @@
           <td>{{ book.bookname }}</td>
           <td>{{ book.author }}</td>
           <td>{{ book.publisher }}</td>
-          <td v-if="book.purchase > 0">&nbsp;\{{ book.purchase }}-</td>
+          <td v-if="book.purchase > 0" align="right">\{{ book.purchase }}-&nbsp;</td>
           <td v-if="book.purchase == 0">&nbsp;図書館</td>
           <td v-if="book.purchase == null">&nbsp;</td>
           <td>{{ book.genre }}</td>
@@ -30,8 +29,7 @@
         </tr>
       </tbody>
     </table>
-    </div>
-  </v-app>
+  </div>
 </template>
 
 <script setup>
@@ -40,6 +38,7 @@
 </script>
 
 <style>
-  th{ color:#fff; background:#005ab3; }
+  th{ color:#fff; background:#005ab3; position:sticky; top:0; }
   table tr:nth-child(odd){ background:#e6f2ff; }
+  tbody { overflow-x: hidden; overflow-y: scroll; height: 100px; }
 </style>
